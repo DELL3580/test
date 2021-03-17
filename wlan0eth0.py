6b546e79e7c9fff262d71d72298f161ae77e50f9
DELL3580 <80255689+DELL3580@users.noreply.github.com>
2021-03-17 16:26:43 +0200

    com99

--- wlan0eth0.py
+++ wlan0eth0.py
@@ -1,43 +1,33 @@
+
 import os
 import math
 import time
+import subprocess
 import Adafruit_GPIO.SPI as SPI
 import Adafruit_SSD1306
 from PIL import Image
 from PIL import ImageFont
 from PIL import ImageDraw
-import subprocess
 
 # Raspberry Pi pin configuration:
-RST = None     # on the PiOLED this pin isnt used
+RST = None
 # 128x32 display with hardware I2C:
-# disp = Adafruit_SSD1306.SSD1306_128_32(rst=RST)
-# 128x64 display with hardware I2C:
-# disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST)
-# Note you can change the I2C address by passing an i2c_address parameter like:
-disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST, i2c_address=0x3C)
+disp = Adafruit_SSD1306.SSD1306_128_32(rst=RST)
 # Initialize library.
 disp.begin()
+# Get display width and height.
+width = disp.width
+height = disp.height
 # Clear display.
 disp.clear()
 disp.display()
-# Create blank image for drawing.
+# Create image buffer.
 # Make sure to create image with mode '1' for 1-bit color.
-width = disp.width
-height = disp.height
 image = Image.new('1', (width, height))
-# Get drawing object to draw on image.
+# Load default font.
+font = ImageFont.load_default()
+# Create drawing object.
 draw = ImageDraw.Draw(image)
-# Draw a black filled box to clear the image.
-draw.rectangle((0,0,width,height), outline=0, fill=0)
-
-# Draw some shapes.
-# First define some constants to allow easy resizing of shapes.
-padding = -2
-top = padding
-bottom = height-padding
-# Move left to right keeping track of the current x position for drawing shapes.
-x = 0
 
 
 #'\' is used to splite python line
@@ -53,18 +43,16 @@ ssid = os.popen("iwconfig wlan0 \
 print("ssid: " + ssid)
 print("ipaddress: " + ipaddress)
 
-# Load default font.
-font = ImageFont.load_default()
-# Draw a black filled box to clear the image.
-draw.rectangle((0,0,width,height), outline=0, fill=0)
-
 # cmd = "hostname -I | cut -d\' \' -f1"
 # IP = subprocess.check_output(cmd, shell = True )
 #draw.text((x, top),"IP: " + str(IP),  font=font, fill=255)
+
 cmd = "top -bn1 | grep load | awk '{printf \"CPU Load: %.2f\", $(NF-2)}'"
 CPU = subprocess.check_output(cmd, shell = True )
+
 cmd = "free -m | awk 'NR==2{printf \"Mem: %s/%sMB %.2f%%\", $3,$2,$3*100/$2 }'"
 MemUsage = subprocess.check_output(cmd, shell = True )
+
 cmd = "df -h | awk '$NF==\"/\"{printf \"Disk: %d/%dGB %s\", $3,$2,$5}'"
 Disk = subprocess.check_output(cmd, shell = True )
 
@@ -96,7 +84,7 @@ while True:
             x += char_width
             continue
         # Calculate offset from sine wave.
-        y = offset+math.floor(amplitude)
+        y = offset+math.floor(amplitude*math.sin(x/float(width)*2.0*math.pi))
         # Draw text.
         draw.text((x, y), c, font=font, fill=255)
         # Increment x position based on chacacter width.
@@ -112,5 +100,4 @@ while True:
         pos = startpos
     # Pause briefly before drawing next frame.
     time.sleep(0.1)
-    
     
