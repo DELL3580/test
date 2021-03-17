185ccbf30dc4648d0be2e73203c2726198867e73
DELL3580 <80255689+DELL3580@users.noreply.github.com>
2021-03-17 16:58:14 +0200

    com88

--- wlan0eth0.py
+++ wlan0eth0.py
@@ -1,103 +1,113 @@
 
 import os
-import math
 import time
-import subprocess
 import Adafruit_GPIO.SPI as SPI
 import Adafruit_SSD1306
 from PIL import Image
-from PIL import ImageFont
 from PIL import ImageDraw
+from PIL import ImageFont
+import subprocess
 
 # Raspberry Pi pin configuration:
-RST = None
+RST = None     # on the PiOLED this pin isnt used
 # 128x32 display with hardware I2C:
-disp = Adafruit_SSD1306.SSD1306_128_32(rst=RST)
+disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST, i2c_address=0x3C)
 # Initialize library.
 disp.begin()
-# Get display width and height.
-width = disp.width
-height = disp.height
 # Clear display.
 disp.clear()
 disp.display()
-# Create image buffer.
+# Create blank image for drawing.
 # Make sure to create image with mode '1' for 1-bit color.
+width = disp.width
+height = disp.height
 image = Image.new('1', (width, height))
+# Get drawing object to draw on image.
+draw = ImageDraw.Draw(image)
+# Draw a black filled box to clear the image.
+draw.rectangle((0,0,width,height), outline=0, fill=0)
+# Draw some shapes.
+# First define some constants to allow easy resizing of shapes.
+padding = -2
+top = padding
+bottom = height-padding
+# Move left to right keeping track of the current x position for drawing shapes.
+x = 0
 # Load default font.
 font = ImageFont.load_default()
-# Create drawing object.
-draw = ImageDraw.Draw(image)
 
+# Alternatively load a TTF font.  Make sure the .ttf font file is in the same directory as the python script!
+# Some other nice fonts to try: http://www.dafont.com/bitmap.php
+# font = ImageFont.truetype('Minecraftia.ttf', 8)
 
-#'\' is used to splite python line
-ipaddress = os.popen("ifconfig wlan0 \
-                     | grep 'inet addr' \
-                     | awk -F: '{print $2}' \
-                     | awk '{print $1}'").read()
-ssid = os.popen("iwconfig wlan0 \
-                | grep 'ESSID' \
-                | awk '{print $4}' \
-                | awk -F\\\" '{print $2}'").read()
-
-print("ssid: " + ssid)
-print("ipaddress: " + ipaddress)
-
-# cmd = "hostname -I | cut -d\' \' -f1"
-# IP = subprocess.check_output(cmd, shell = True )
-#draw.text((x, top),"IP: " + str(IP),  font=font, fill=255)
-
-cmd = "top -bn1 | grep load | awk '{printf \"CPU Load: %.2f\", $(NF-2)}'"
-CPU = subprocess.check_output(cmd, shell = True )
-
-cmd = "free -m | awk 'NR==2{printf \"Mem: %s/%sMB %.2f%%\", $3,$2,$3*100/$2 }'"
-MemUsage = subprocess.check_output(cmd, shell = True )
-
-cmd = "df -h | awk '$NF==\"/\"{printf \"Disk: %d/%dGB %s\", $3,$2,$5}'"
-Disk = subprocess.check_output(cmd, shell = True )
-
-# Define text and get total width.
-text = "ssid: " + str(ssid) +"ipaddress: " + str(ipaddress) + "CPU" + str(CPU) + "MemUsage"+str(MemUsage) +"Disk"+str(Disk)
-maxwidth, unused = draw.textsize(text, font=font)
-
-# Set animation and sine wave parameters.
-amplitude = height/4
-offset = height/2 - 4
-velocity = -2
-startpos = width
-
-# Animate text moving in sine wave.
-print('Press Ctrl-C to quit.')
-pos = startpos
 while True:
-    # Clear image buffer by drawing a black filled box.
+
+        #'\' is used to splite python line
+    ipaddress = os.popen("ifconfig wlan0 \
+                        | grep 'inet addr' \
+                        | awk -F: '{print $2}' \
+                        | awk '{print $1}'").read()
+    ssid = os.popen("iwconfig wlan0 \
+                    | grep 'ESSID' \
+                    | awk '{print $4}' \
+                    | awk -F\\\" '{print $2}'").read()
+
+    print("ssid: " + ssid)
+    print("ipaddress: " + ipaddress)
+
+    # Draw a black filled box to clear the image.
     draw.rectangle((0,0,width,height), outline=0, fill=0)
-    # Enumerate characters and draw them offset vertically based on a sine wave.
-    x = pos
-    for i, c in enumerate(text):
-        # Stop drawing if off the right side of screen.
-        if x > width:
-            break
-        # Calculate width but skip drawing if off the left side of screen.
-        if x < -10:
-            char_width, char_height = draw.textsize(c, font=font)
-            x += char_width
-            continue
-        # Calculate offset from sine wave.
-        y = offset+math.floor(amplitude*math.sin(x/float(width)*2.0*math.pi))
-        # Draw text.
-        draw.text((x, y), c, font=font, fill=255)
-        # Increment x position based on chacacter width.
-        char_width, char_height = draw.textsize(c, font=font)
-        x += char_width
-    # Draw the image buffer.
+
+    cmd = "hostname -I | cut -d\' \' -f1"
+    IP = subprocess.check_output(cmd, shell = True )
+
+    cmd = "top -bn1 | grep load | awk '{printf \"CPU Load: %.2f\", $(NF-2)}'"
+    CPU = subprocess.check_output(cmd, shell = True )
+
+    cmd = "free -m | awk 'NR==2{printf \"Mem: %s/%sMB %.2f%%\", $3,$2,$3*100/$2 }'"
+    MemUsage = subprocess.check_output(cmd, shell = True )
+
+    cmd = "df -h | awk '$NF==\"/\"{printf \"Disk: %d/%dGB %s\", $3,$2,$5}'"
+    Disk = subprocess.check_output(cmd, shell = True )
+
+    # Write two lines of text.
+    draw.text((x, top),       "ssid: " + str(ssid),  font=font, fill=255)
+    # Display image.
     disp.image(image)
     disp.display()
-    # Move position for next frame.
-    pos += velocity
-    # Start over if text has scrolled completely off left side of screen.
-    if pos < -maxwidth:
-        pos = startpos
-    # Pause briefly before drawing next frame.
-    time.sleep(0.1)
+    time.sleep(1.5)
+    # Clear display.
+    disp.clear()
+    disp.display()
+
+    draw.text((x, top),       "IP: " + str(IP),  font=font, fill=255)
+    # Display image.
+    disp.image(image)
+    disp.display()
+    time.sleep(1.5)
+    # Clear display.
+    disp.clear()
+    disp.display()
+    
+    draw.text((x, top+8),     str(CPU), font=font, fill=255)
+       # Display image.
+    disp.image(image)
+    disp.display()
+    time.sleep(1.5)
+    # Clear display.
+    disp.clear()
+    disp.display()
+    draw.text((x, top+16),    str(MemUsage),  font=font, fill=255)
+       # Display image.
+    disp.image(image)
+    disp.display()
+    time.sleep(1.5)
+    # Clear display.
+    disp.clear()
+    disp.display()
+    draw.text((x, top+25),    str(Disk),  font=font, fill=255)
+       # Display image.
+    disp.image(image)
+    disp.display()
+    time.sleep(1.5)
     
