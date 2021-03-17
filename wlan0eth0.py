e99b55097c958f6ec607f98c8d66afe73774d4ff
DELL3580 <80255689+DELL3580@users.noreply.github.com>
2021-03-17 15:39:27 +0200

    com3

--- wlan0eth0.py
+++ wlan0eth0.py
@@ -8,41 +8,26 @@ from PIL import ImageFont
 from PIL import ImageDraw
 import subprocess
 
-
-
 # Raspberry Pi pin configuration:
 RST = None     # on the PiOLED this pin isnt used
-
 # 128x32 display with hardware I2C:
-disp = Adafruit_SSD1306.SSD1306_128_32(rst=RST)
-
+# disp = Adafruit_SSD1306.SSD1306_128_32(rst=RST)
 # 128x64 display with hardware I2C:
 # disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST)
-
 # Note you can change the I2C address by passing an i2c_address parameter like:
-# disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST, i2c_address=0x3C)
-
-# Alternatively you can specify an explicit I2C bus number, for example
-# with the 128x32 display you would use:
-# disp = Adafruit_SSD1306.SSD1306_128_32(rst=RST, i2c_bus=2)
-
-
+disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST, i2c_address=0x3C)
 # Initialize library.
 disp.begin()
-
 # Clear display.
 disp.clear()
 disp.display()
-
 # Create blank image for drawing.
 # Make sure to create image with mode '1' for 1-bit color.
 width = disp.width
 height = disp.height
 image = Image.new('1', (width, height))
-
 # Get drawing object to draw on image.
 draw = ImageDraw.Draw(image)
-
 # Draw a black filled box to clear the image.
 draw.rectangle((0,0,width,height), outline=0, fill=0)
 
@@ -55,7 +40,7 @@ bottom = height-padding
 x = 0
 
 
-#'\' is used to splite pythone line
+#'\' is used to splite python line
 ipaddress = os.popen("ifconfig wlan0 \
                      | grep 'inet addr' \
                      | awk -F: '{print $2}' \
@@ -68,23 +53,14 @@ ssid = os.popen("iwconfig wlan0 \
 print("ssid: " + ssid)
 print("ipaddress: " + ipaddress)
 
-# Define text and get total width.
-#text = "ssid:"+ ssid , "ipaddress:"+ipaddress
-#maxwidth, unused = draw.textsize(text, font=font)
-
 # Load default font.
 font = ImageFont.load_default()
-
-# Alternatively load a TTF font.  Make sure the .ttf font file is in the same directory as the python script!
-# Some other nice fonts to try: http://www.dafont.com/bitmap.php
-# font = ImageFont.truetype('Minecraftia.ttf', 8)
 
 while True:
 
     # Draw a black filled box to clear the image.
     draw.rectangle((0,0,width,height), outline=0, fill=0)
 
-    # Shell scripts for system monitoring from here : https://unix.stackexchange.com/questions/119126/command-to-display-memory-usage-disk-usage-and-cpu-load
     cmd = "hostname -I | cut -d\' \' -f1"
     IP = subprocess.check_output(cmd, shell = True )
     
@@ -97,7 +73,7 @@ while True:
     # Display image.
     disp.image(image)
     disp.display()
-    time.sleep(1)
+    time.sleep(.1)
 
     # Clear display.
     disp.clear()
@@ -106,36 +82,49 @@ while True:
 
     cmd = "free -m | awk 'NR==2{printf \"Mem: %s/%sMB %.2f%%\", $3,$2,$3*100/$2 }'"
     MemUsage = subprocess.check_output(cmd, shell = True )
-    
-
-    # Write two lines of text.
-
-    draw.text((x, top),       "MemUsage: " + str(MemUsage),  font=font, fill=255)
+    draw.text((x, top),"MemUsage: " + str(MemUsage),  font=font, fill=255)
     
 
     # Display image.
     disp.image(image)
     disp.display()
-    time.sleep(1)
+    time.sleep(.1)
 
-    # Clear display.
-    disp.clear()
-    disp.display()
+    # # Clear display.
+    # disp.clear()
 
-
-    cmd = "df -h | awk '$NF==\"/\"{printf \"Disk: %d/%dGB %s\", $3,$2,$5}'"
-    Disk = subprocess.check_output(cmd, shell = True )
+    # cmd = "df -h | awk '$NF==\"/\"{printf \"Disk: %d/%dGB %s\", $3,$2,$5}'"
+    # Disk = subprocess.check_output(cmd, shell = True )
     
 
-    # Write two lines of text.
+    # # Write two lines of text.
 
-    draw.text((x, top),       "Disk: " + str(Disk),  font=font, fill=255)
+    # draw.text((x, top),       "Disk: " + str(Disk),  font=font, fill=255)
     
 
-    # Display image.
-    disp.image(image)
-    disp.display()
-    time.sleep(1)
+    # # Display image.
+    # disp.image(image)
+    # disp.display()
+    # time.sleep(.1)
+
+    # # Clear display.
+    # disp.clear()
+    # disp.display()
+
+
+    #  cmd = "hostname -I | cut -d\' \' -f1"
+    # IP = subprocess.check_output(cmd, shell = True )
+    
+
+    # # Write two lines of text.
+
+    # draw.text((x, top),       "IP: " + str(IP),  font=font, fill=255)
+    
+
+    # # Display image.
+    # disp.image(image)
+    # disp.display()
+    # time.sleep(.1)
 
     
     
